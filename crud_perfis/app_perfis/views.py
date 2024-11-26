a13c005c7c2dd2e3c9b201973bf18d3c9e1342ad
ligiaboliveira <ligiabdeoliveira@gmail.com>
2024-11-26 14:21:03 -0300

    ✨ implementado login e logout, e sessão com jwt

--- crud_perfis/app_perfis/views.py
+++ crud_perfis/app_perfis/views.py
@@ -1,12 +1,18 @@
+import jwt
+from django.conf import settings
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.serializers import Serializer, CharField
-
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import logout
+from django.http import HttpResponseForbidden
+from django.http import JsonResponse
 
 class CustomTokenObtainPairView(APIView):
     permission_classes = [AllowAny]
@@ -49,3 +55,57 @@ class UserRegistrationSerializer(Serializer):
         if data['password'] != data['password2']:
             raise serializers.ValidationError("As senhas não coincidem.")
         return data
+    
+def is_valid_jwt(token):
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+    return payload
+
+def validate_token(request):
+    token = request.data.get('access_token')
+    print('token')
+    print(token)
+    if token:
+        payload = is_valid_jwt(token)
+        if payload is None:
+            return JsonResponse({'message': 'Token inválido ou expirado'}, status=403)
+        return JsonResponse({'message': 'Token válido'}, status=200)
+    
+    return JsonResponse({'message': 'Token não fornecido'}, status=400)
+    
+def home_view(request, access_token=None):
+    return render(request, 'home.html')
+    
+def login_view(request):
+    if request.method == 'GET':
+        # Renderiza a página de login
+        return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            access_token = refresh.access_token
+            return JsonResponse({
+                'access_token': str(access_token),
+                'refresh_token': str(refresh)
+            }, status=200)
+
+        return JsonResponse({'error': 'Credenciais inválidas'}, status=400)
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "Dados protegidos acessados com sucesso!"})
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # Invalidar a sessão no Django (remove o usuário da sessão)
+        logout(request)
+
+        # Resposta de sucesso após logout
+        return Response({"message": "Logout realizado com sucesso!"}, status=status.HTTP_200_OK)
--- crud_perfis/crud_perfis/urls.py
+++ crud_perfis/crud_perfis/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from app_perfis.views import CustomTokenObtainPairView, UserRegistrationView
+from app_perfis.views import CustomTokenObtainPairView, UserRegistrationView,  login_view, ProtectedView, LogoutView, home_view, validate_token
 
 urlpatterns = [
     path('api/token', CustomTokenObtainPairView.as_view(), name='token_obtain_pair_custom'),    
     path('api/register', UserRegistrationView.as_view(), name='user_register'),
+    path('login/', login_view, name='login'),
+    path('home/', home_view, name='home'),  # A home vai passar o JWT como parte do template
+    path('api/protected/', ProtectedView.as_view(), name='protected'),
+    path('api/logout/', LogoutView.as_view(), name='logout'),  # Rota para logout
+    path('api/validate_token', validate_token, name='validate_token'),  # Endpoint para validar o token
 ]
